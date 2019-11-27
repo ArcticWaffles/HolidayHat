a1e4a15e141ffde744d64a99e123b67793b68017
Tisa Foster <tisa.foster@gmail.com>
2019-11-26 20:14:43 -0700

    Avoid repeating last year's matches

--- ChristmasHat/Program.cs
+++ ChristmasHat/Program.cs
@@ -21,22 +21,29 @@ namespace ChristmasHat
 
         static void Main(string[] args)
         {
+            var results2019 = new Dictionary<Person, Person>()
+            {
+                {Person.Christie, Person.Andrew },
+                {Person.Andrew, Person.Ron },
+                {Person.Ron, Person.Tisa },
+                {Person.Tisa, Person.Christie },
+                {Person.Dianne, Person.John },
+                {Person.John, Person.Dianne },
+            };
+
             var people = Enum.GetValues(typeof(Person)).Cast<Person>();
 
             Dictionary<Person, Person> results = null;
 
             while (results == null)
-            {
-                results = AssignRecipients(people);
-            }
+                results = AssignRecipients(people, results2019);
 
             foreach (Person person in people)
-            {
                 Debug.WriteLine($"{person} is gifting to {results[person]}");
-            }
         }
 
-        static Dictionary<Person, Person> AssignRecipients(IEnumerable<Person> people)
+        static Dictionary<Person, Person> AssignRecipients(IEnumerable<Person> people,
+                                                           Dictionary<Person, Person> lastYearsResults)
         {
             var random = new Random();
             var gifters = new List<Person>(people);
@@ -49,12 +56,11 @@ namespace ChristmasHat
                 do
                 {
                     if (recipients.Count == 1 && recipients[0] == gifter)
-                    {
                         return null;
-                    }
+
                     recipient = recipients[random.Next(recipients.Count)];
                 }
-                while (recipient == gifter);
+                while (recipient == gifter || lastYearsResults[gifter] == recipient);
                 recipients.Remove(recipient);
                 results.Add(gifter, recipient);
             }
