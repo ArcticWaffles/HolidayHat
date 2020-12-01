a34f377bf83be539cbdd69a83a879396a9a90471
Tisa Foster <tisa.foster@gmail.com>
2020-11-30 21:10:23 -0700

    Name change

--- HolidayHat/Properties/AssemblyInfo.cs
+++ HolidayHat/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChristmasHat")]
+[assembly: AssemblyTitle("HolidayHat")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChristmasHat")]
+[assembly: AssemblyProduct("HolidayHat")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
